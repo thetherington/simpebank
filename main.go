9781d65a2a84a723ad42034c996889ebadfb771a
thetherington <thomas@hetheringtons.org>
2023-06-15 13:35:50 -0400

    grpc gateway http rest api

--- main.go
+++ main.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+	"context"
 	"database/sql"
 	"log"
 	"net"
+	"net/http"
 
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	_ "github.com/lib/pq"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
+	"google.golang.org/protobuf/encoding/protojson"
 
 	"github.com/thetherington/simplebank/api"
 	db "github.com/thetherington/simplebank/db/sqlc"
@@ -28,6 +32,8 @@ func main() {
 	}
 
 	store := db.NewStore(conn)
+
+	go runGatewayServer(config, store)
 
 	runGrpcServer(config, store)
 }
@@ -63,6 +69,49 @@ func runGrpcServer(config util.Config, store db.Store) {
 	log.Printf("start gRPC server at %s", listener.Addr().String())
 
 	err = grpcServer.Serve(listener)
+	if err != nil {
+		log.Fatal("cannot start gRPC Server")
+	}
+}
+
+func runGatewayServer(config util.Config, store db.Store) {
+	server, err := gapi.NewServer(config, store)
+	if err != nil {
+		log.Fatal("cannot create server: ", err)
+	}
+
+	// make the grpc gateway use the snake case names defined in proto files
+	jsonOption := runtime.WithMarshalerOption(runtime.MIMEWildcard, &runtime.JSONPb{
+		MarshalOptions: protojson.MarshalOptions{
+			UseProtoNames: true,
+		},
+		UnmarshalOptions: protojson.UnmarshalOptions{
+			DiscardUnknown: true,
+		},
+	})
+
+	grpcMux := runtime.NewServeMux(jsonOption)
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	// from the service_simple_bank.pb.gw.go
+	err = pb.RegisterSimpleBankHandlerServer(ctx, grpcMux, server)
+	if err != nil {
+		log.Fatal("cannot register handler server")
+	}
+
+	mux := http.NewServeMux()
+	mux.Handle("/", grpcMux)
+
+	listener, err := net.Listen("tcp", config.HTTPServerAddress)
+	if err != nil {
+		log.Fatal("cannot create listener")
+	}
+
+	log.Printf("start HTTP gateway server at %s", listener.Addr().String())
+
+	err = http.Serve(listener, mux)
 	if err != nil {
 		log.Fatal("cannot start gRPC Server")
 	}
